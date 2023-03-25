81a00184c37c32ae7f23dd55852405f42957365b
Ti Arman <todevarman@gmail.com>
2023-03-25 21:31:58 +0600

    doctors load completed

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     public function doctors($department_id) {
-        $data['districts'] = Doctor::where("department_id", $department_id)
+        $data['doctors'] = Doctor::where("department_id", $department_id)
             ->get(["name", "id"]);
 
         return response()->json($data);
